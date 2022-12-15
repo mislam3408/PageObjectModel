21d462bccea4aa4637b7865cd4a3a9e40b1130ac
Mumin Islam <mislam1049@gmail.com>
2022-12-14 20:54:10 -0500

    new test

---
+++ src/test/java/com/crm/qa/testcases/TestCase.java
@@ -0,0 +1,15 @@
+package com.crm.qa.testcases;
+
+import org.testng.reporters.jq.Main;
+
+public class TestCase {
+
+
+    public static void main(String[] args){
+
+        System.out.println("Hello, World!");
+
+    }
+
+
+}
